3b73758e6d112e1c47feb92dee725c320556b7c0
Alfiya <k.alfiya217@gmail.com>
2023-07-30 09:32:29 +0530

    all code

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponentComponent } from './home-component/home-component.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    pathMatch: 'full',
+    component: HomeComponentComponent
+  },
+
+  {
+    path: "**",
+    component: HomeComponentComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
