439c50b3d611f86b143ac7c0292487b01ca85f61
Alfiya <k.alfiya217@gmail.com>
2023-08-03 15:13:09 +0530

    slided added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponentComponent } from './home-component/home-component.component';
+import { DragScrollModule } from 'ngx-drag-scroll';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { HomeComponentComponent } from './home-component/home-component.componen
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    DragScrollModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/home-component/home-component.component.ts
+++ src/app/home-component/home-component.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
+import { ViewportScroller } from '@angular/common';
+// import data from '../../assets/data/products.json';
 
 @Component({
   selector: 'app-home-component',
@@ -7,4 +9,75 @@ import { Component } from '@angular/core';
 })
 export class HomeComponentComponent {
 
+  productList: Product[] = [
+    {
+      "title": "Groom to Perfection: Aloe Vera Hair Mask for Men's Hair Care",
+      "bytitle" : "By Mars by GHC on Jun 22, 2022",
+      "image": "../../assets/slider2/img1.png"
+    },
+    {
+      "title": "Coffee for Hair Growth: Brew Up Longer, Thicker Hair with Coffee",
+      "bytitle" : "By Mars by GHC on Jun 22, 2022",
+      "image": "../../assets/slider2/img2.png"
+    },
+    {
+      "title": "Maximizing Hair Growth and Health: 7 Benefits of Biotin Hair Gummies",
+      "bytitle" : "By Mars by GHC on Jun 22, 2022",
+      "image": "../../assets/slider2/img3.png"
+    },
+    {
+      "title": "Vitamin E tabs for hair: Get Lustrous Hair In No Time!",
+      "bytitle" : "By Mars by GHC on Jun 22, 2022",
+      "image": "../../assets/slider2/img4.png"
+    }
+  ];
+  // jsonData = data as Array<any>;
+  direction = "";
+
+  public isCollapsed = false;
+
+  isNavCollapse = false;
+
+  constructor(private scroll: ViewportScroller) { }
+
+  @HostListener('window:scroll', []) onScroll() {
+    if (this.scroll.getScrollPosition()[1] > 70) {
+      this.isNavCollapse = true;
+    } else {
+      this.isNavCollapse = false;
+    }
+  }
+
+  ngOnInit() {
+    // for (let i = 0; i < 9; i++) {
+    //   let product = new Product(this.jsonData[i]);
+    //   this.productList.push(product);
+    // }
+  }
+
+  onWheel(event: WheelEvent): void {
+    if (event.deltaY > 0) this.scrollToRight();
+    // else this.scrollToLeft();
+  }
+
+  // scrollToLeft(): void {
+  //   document.getElementById('scroll-1').scrollLeft -= 400;
+  // }
+
+  scrollToRight(): void {
+    document.getElementById('scroll-1')!.scrollLeft += 400;
+  }
+  
+}
+
+class Product {
+  title: string;
+  image: string;
+  bytitle: string;
+
+  constructor(product: any = {}) {
+    this.title = product.title;
+    this.bytitle = product.bytitle;
+    this.image = 'https://alcodesbase.blob.core.windows.net/generic/sections-default-image.png';
+  }
 }
